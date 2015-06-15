e4022b268c8dee2b38180456c656db05bc646b58
Soren Peterson <soren468@gmail.com>
2015-06-14 21:21:58 -0700

    remove debugging console.log()'s

--- topscope.js
+++ topscope.js
@@ -1,7 +1,4 @@
 // Write your package code here!
-console.log('hi');
-console.log(Meteor.isClient);
-console.log(Meteor.isServer);
 if(Meteor.isClient) {
 	window.topScope = window;
 } else if (Meteor.isServer) {
