e94d3722420c32ce08808cadbae60e2c71e9afb5
Soren Peterson <soren468@gmail.com>
2015-06-14 21:14:03 -0700

    Fix bug preventing server from being configured correctly

--- topscope.js
+++ topscope.js
@@ -1,6 +1,9 @@
 // Write your package code here!
+console.log('hi');
+console.log(Meteor.isClient);
+console.log(Meteor.isServer);
 if(Meteor.isClient) {
 	window.topScope = window;
-} else if (Meteor.isServert) {
+} else if (Meteor.isServer) {
 	global.topScope = global;
 }
